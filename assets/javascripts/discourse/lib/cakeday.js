030e8023daef32e534342dd8ea8cd83bf83b3505
DaVania <github_ivi@miransky.dk>
2022-04-13 01:12:39 +0200

    Update cakeday.js

--- assets/javascripts/discourse/lib/cakeday.js
+++ assets/javascripts/discourse/lib/cakeday.js
@@ -32,22 +32,11 @@ export function cakedayBirthday(dateOfBirth) {
 }
 
 export function userAge(dateOfBirth) {
-  return dateOfBirth
-<<<<<<< HEAD
-      ? (moment(dateOfBirth, "YYYY-MM-DD").year() !== 1904 ? moment().diff(dateOfBirth, 'years') : null)
-      : null;
+  return dateOfBirth ? (moment(dateOfBirth, "YYYY-MM-DD").year() !== 1904 ? moment().diff(dateOfBirth, 'years') : null) : null;
 }
 
 export function userAgeTitle(user) {
   return (user.date_of_birth && moment(user.date_of_birth, "YYYY-MM-DD").year() !== 1904) ? I18n.t("cakeday.age", {age: userAge(user.date_of_birth)}) : null;
-=======
-      ? (moment(dateOfBirth, "YYYY-MM-DD").year() != 1904 ? moment().diff(dateOfBirth, 'years') : null)
-      : null;
-}
-
-export function userAgeTitle(user, currentUser) {
-  return (user.date_of_birth && moment(user.date_of_birth, "YYYY-MM-DD").year() != 1904) ? I18n.t("cakeday.age", {age: userAge(user.date_of_birth)}) : null;
->>>>>>> eb4b8a7a696c127871a8a345c83f12d1121ab1f0
 }
 
 export function cakedayTitle(user, currentUser) {
